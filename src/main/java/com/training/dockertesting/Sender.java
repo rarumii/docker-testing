07d3c820ad4767e255f5c8c7ebed8763f94f0dca
miquel <miquel.auma@gmail.com>
2021-04-06 00:38:41 +0200

    feat: add basic sender

---
+++ src/main/java/com/training/dockertesting/Sender.java
@@ -0,0 +1,34 @@
+package com.training.dockertesting;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+
+    private final static String QUEUE_NAME = "queue-test";
+
+    public static void main(String[] argv){
+        publish();
+    }
+
+    private static void publish(){
+
+        try (Connection connection = Broker.getConnection();
+             Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
